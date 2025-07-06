8d0ef2d2df595a8b1aa3cdf3eda9578786facb43
Nithyasri R <122379760+Nithyasri-12@users.noreply.github.com>
2025-07-06 20:26:11 +0530

    Add files via upload

---
+++ Week 1/Week 1_Design principles & Patterns/2_Implementing the Factory Method Pattern/code/ExcelDocument.java
@@ -0,0 +1,10 @@
+package documents;
+
+public class ExcelDocument implements Document {
+	@Override
+	public void open() {
+		System.out.println("Opening an Excel document.");
+	}
+
+}
+
---
+++ Week 1/Week 1_Design principles & Patterns/2_Implementing the Factory Method Pattern/code/ExcelDocumentFactory.java
@@ -0,0 +1,11 @@
+package documents;
+
+public class ExcelDocumentFactory extends DocumentFactory {
+
+	@Override
+	public Document createDocument() {
+		return new ExcelDocument();
+	}
+
+}
+
