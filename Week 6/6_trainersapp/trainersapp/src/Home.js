ccaedee7a17ee71c047d1ce2e001bcb845531ac8
Nithyasri R <122379760+Nithyasri-12@users.noreply.github.com>
2025-07-27 13:17:36 +0530

    Add files via upload

---
+++ Week 6/6_trainersapp/trainersapp/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h2>Welcome to Cognizant Academy</h2>
+    <p>This is the trainer management portal.</p>
+  </div>
+);
+
+export default Home;
